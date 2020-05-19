fb3ca268ee700b21d532e7c0fdaeb23dce7e8f56
Vinicius Vieira Abreu <31490903+vncius@users.noreply.github.com>
2020-05-18 21:04:16 -0300

    Aula 6.1 - Estruturas de decisão

--- Modulo 1/Aulas/Aula05/js/script.js
+++ Modulo 1/Aulas/Aula05/js/script.js
@@ -2,3 +2,15 @@ console.log('Olá, Mundo!');
 
 var title = document.querySelector('h1');
 title.textContent = 'Modificado via JS por VnC';
+
+var r = '';
+
+switch (dia) {
+  case 1:
+    r = 'Segunda, dia ' + dia;
+    break;
+
+  case 2:
+    r = 'Terça, dia ' + dia;
+    break;
+}
---
+++ Modulo 1/Aulas/Aula06/js/script.js
@@ -0,0 +1,4 @@
+console.log('Olá, Mundo!');
+
+var title = document.querySelector('h1');
+title.textContent = 'Modificado via JS por VnC';
