52e336fb42df27c4d11fd2b75493660de4516fc3
Vinicius Vieira Abreu <31490903+vncius@users.noreply.github.com>
2020-05-19 13:09:30 -0300

    Aula 7.1 - Manipulando o DOM com JavaScript

---
+++ Modulo 1/Aulas/Aula07/js/script.js
@@ -0,0 +1,10 @@
+// var data = Array.from(document.querySelectorAll('.data'));
+
+// for (var i = 0; i < data.length; i++) {
+//   var currentElement = data[i];
+//   currentElement.classList.add('emphasis');
+// }
+
+var p = document.querySelector('p');
+
+p.style.color = 'blue';
