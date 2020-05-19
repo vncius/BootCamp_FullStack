64461c7f225b154fad6b30813286b5314b4a9008
Vinicius Vieira Abreu <31490903+vncius@users.noreply.github.com>
2020-05-18 21:20:35 -0300

    Aula 6.1 - Estruturas de decisão

--- Modulo 1/Aulas/Aula06/js/script.js
+++ Modulo 1/Aulas/Aula06/js/script.js
@@ -2,3 +2,36 @@ console.log('Olá, Mundo!');
 
 var title = document.querySelector('h1');
 title.textContent = 'Modificado via JS por VnC';
+
+var r = '';
+
+//SWITCH AO INVÉS DE IF FICA MAIS LEGIVEL
+var dia = 2;
+
+switch (dia) {
+  case 1:
+    r = 'Segunda, dia ' + dia;
+    break;
+  case 2:
+    r = 'Terça, dia ' + dia;
+    break;
+}
+
+//IF TERNÁRIO
+var a = 11;
+var b = 10;
+var resultado = 0;
+
+if (a > b) {
+  resultado = a + b;
+} else {
+  resultado = a - b;
+}
+
+console.log('Resultado IF normal: ' + resultado);
+
+resultado = 0;
+resultado = a > b ? a + b : a - b;
+
+console.log('Resultado IF ternário: ' + resultado);
+
