b6570ccf348cd34adbec822cf739724e5a513918
Vinicius Vieira Abreu <31490903+vncius@users.noreply.github.com>
2020-06-03 14:32:49 -0300

    Deletado projetos desenvolvidos durantes aulas.

--- Modulo 2/Aulas/Endpoint-Básico/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello word 123'));
-
-app.listen(port, () => {
-  console.log(`App listerning on port ${port}`);
-});
