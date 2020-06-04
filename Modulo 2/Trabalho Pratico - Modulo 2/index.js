f0e5a397ece78291ea0d92ad40f928eb2e98c6e9
Vinicius Vieira Abreu <31490903+vncius@users.noreply.github.com>
2020-06-03 21:21:08 -0300

    Implementado e configurado logs de aplicação utilizando winston

--- Modulo 2/Trabalho Pratico - Modulo 2/index.js
+++ Modulo 2/Trabalho Pratico - Modulo 2/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app = express();
+const winston = require('winston');
 const routerState = require('./src/routes/routeStates.js');
 const controllerState = require('./src/controller/controllerStates.js');
+
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+global.logger = winston.createLogger({
+  level: 'silly',
+  //prettier-ignore
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({filename: './Logs/API.log'})
+  ],
+  format: combine(label({ label: 'api' }), timestamp(), myFormat),
+});
 
 app.use(express.json());
 app.use('/state', routerState);
 
 app.listen(3000, async () => {
-  console.log('listerning');
-  await controllerState.init();
+  logger.info('listerning at port 3000...');
+  try {
+    await controllerState.init();
+  } catch (error) {
+    logger.error(error);
+  }
 });
--- Modulo 2/Trabalho Pratico - Modulo 2/src/routes/routeStates.js
+++ Modulo 2/Trabalho Pratico - Modulo 2/src/routes/routeStates.js
@@ -4,56 +4,74 @@ const control = require('../controller/controllerStates.js');
 
 router.get('/QtdCidadesPorEstado/:uf', async (req, res) => {
   try {
+    logger.info(`GET url(${req.url}) - ip(${req.ip})`);
     res.send(await control.obtenhaQuatidadeDeCidadesPorEstado(req.params.uf));
   } catch (err) {
+    logger.error(`GET /QtdCidadesPorEstado - IP(${req.ip}) - ${err}`);
     res.status(400).send({ error: err });
   }
 });
 
-router.get('/obtenhaEstadosComCidadesDeMaiorNome', async (_req, res) => {
+router.get('/obtenhaEstadosComCidadesDeMaiorNome', async (req, res) => {
   try {
+    logger.info(`GET url(${req.url}) - ip(${req.ip})`);
     res.send(await control.obtenhaEstadosECidadePorRegra(true));
   } catch (err) {
+    //prettier-ignore
+    logger.error(`GET /obtenhaEstadosComCidadesDeMaiorNome - IP(${req.ip}) - ${err}`);
     res.status(400).send({ error: err });
   }
 });
 
-router.get('/obtenhaEstadosComCidadesDeMenorNome', async (_req, res) => {
+router.get('/obtenhaEstadosComCidadesDeMenorNome', async (req, res) => {
   try {
+    logger.info(`GET url(${req.url}) - ip(${req.ip})`);
     res.send(await control.obtenhaEstadosECidadePorRegra(false));
   } catch (err) {
+    //prettier-ignore
+    logger.error(`GET /obtenhaEstadosComCidadesDeMenorNome - IP(${req.ip}) - ${err}`);
     res.status(400).send({ error: err });
   }
 });
 
-router.get('/obtenhaCidadesOrdenadasMaiorNome', async (_req, res) => {
+router.get('/obtenhaCidadesOrdenadasMaiorNome', async (req, res) => {
   try {
+    logger.info(`GET url(${req.url}) - ip(${req.ip})`);
     res.send(await control.obtenhaCidadesOrdenadasPorNome(true));
   } catch (err) {
+    //prettier-ignore
+    logger.error(`GET /obtenhaCidadesOrdenadasMaiorNome - IP(${req.ip}) - ${err}`);
     res.status(400).send({ error: err });
   }
 });
 
-router.get('/obtenhaCidadesOrdenadasMenorNome', async (_req, res) => {
+router.get('/obtenhaCidadesOrdenadasMenorNome', async (req, res) => {
   try {
+    logger.info(`GET url(${req.url}) - ip(${req.ip})`);
     res.send(await control.obtenhaCidadesOrdenadasPorNome(false));
   } catch (err) {
+    //prettier-ignore
+    logger.error(`GET /obtenhaCidadesOrdenadasMenorNome - IP(${req.ip}) - ${err}`);
     res.status(400).send({ error: err });
   }
 });
 
-router.get('/obtenhaEstadosComMaisCidades', async (_req, res) => {
+router.get('/obtenhaEstadosComMaisCidades', async (req, res) => {
   try {
+    logger.info(`GET url(${req.url}) - ip(${req.ip})`);
     res.send(await control.obtenhaTop5Estados(true));
   } catch (err) {
+    logger.error(`GET /obtenhaEstadosComMaisCidades - IP(${req.ip}) - ${err}`);
     res.status(400).send({ error: err });
   }
 });
 
-router.get('/obtenhaEstadosComMenosCidades', async (_req, res) => {
+router.get('/obtenhaEstadosComMenosCidades', async (req, res) => {
   try {
+    logger.info(`GET url(${req.url}) - ip(${req.ip})`);
     res.send(await control.obtenhaTop5Estados(false));
   } catch (err) {
+    logger.error(`GET /obtenhaEstadosComMenosCidades - IP(${req.ip}) - ${err}`);
     res.status(400).send({ error: err });
   }
 });
